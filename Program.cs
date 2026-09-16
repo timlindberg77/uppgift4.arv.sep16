dfca6395deace265eb37eff0a75b307250768ee6
timlindberg77 <tim.lindberg888@gmail.com>
2026-09-16 11:01:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace uppgift4.arv.sep16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
